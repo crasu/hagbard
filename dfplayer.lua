016958576b86d6c922a7a807fcbbe2f69549b118
Christian Krämer <christian.kraemer@tngtech.com>
2017-09-28 22:56:27 +0200

    added functions

--- dfplayer.lua
+++ dfplayer.lua
@@ -4,16 +4,24 @@ local bit = require("bit")
 
 local VERSION = 0xFF
 local LENGTH = 0x06
-local FEEDBACK = 0x01
+local FEEDBACK = 0x00
+local DEBUG = 1
+
+dfplayer.EQ_NORMAL = 1
+dfplayer.EQ_POP = 2
+dfplayer.EQ_ROCK = 3
+dfplayer.EQ_JAZZ = 4
+dfplayer.EQ_CLASSIC = 5
+dfplayer.EQ_BASE = 6
 
 
 function dfplayer.init(tx_pin, rx_pin, busy_pin)
-    softuart.setup(9600, tx_pin, tx_pin);
+    softuart.setup(9600, tx_pin, rx_pin);
     softuart.on("data", 1, function(data) print("lua handler called"); print(data) end);
 end
 
 function dfplayer.init_default()
-    dfplayer.init(5, 4, 16)
+    dfplayer.init(4, 5, 16)
 end
 
 function dfplayer.print_table(table)
@@ -42,22 +50,57 @@ end
 
 function dfplayer.send_seq(seq)
     for _, s in pairs(seq) do
-        print(string.format("%x", s))
+        if DEBUG then
+            print(string.format("Send byte: %x", s))
+        end
         softuart.write(s);
     end
 end
 
 function dfplayer.next()
-    dfplayer.send_command(0x01, 00, 00)
+    dfplayer.send_command(0x01, 0, 0)
 end
 
 function dfplayer.prev()
-    dfplayer.send_command(0x02, 00, 00)
+    dfplayer.send_command(0x02, 0, 0)
 end
 
 function dfplayer.play(track)
     low, high = dfplayer.split_number(track)
     dfplayer.send_command(0x03, low, high)
+end
+
+function dfplayer.volume_up()
+    dfplayer.send_command(0x04, 0, 0)
+end
+
+function dfplayer.volume_down()
+    dfplayer.send_command(0x05, 0, 0)
+end
+
+function dfplayer.volume(track)
+    low, high = dfplayer.split_number(track)
+    dfplayer.send_command(0x06, low, high)
+end
+
+function dfplayer.set_equalizer_mode(mode)
+    if mode > 6 then
+        print("Mode " .. mode .. " out of range")
+        return
+    end
+    dfplayer.send_command(0x07, 0, mode)
+end
+
+function dfplayer.playback()
+    dfplayer.send_command(0x0D, 0, 0)
+end
+
+function dfplayer.playback()
+    dfplayer.send_command(0x0D, 0, 0)
+end
+
+function dfplayer.pause()
+    dfplayer.send_command(0x0E, 0, 0)
 end
 
 return dfplayer
