0b56d245214c64c7fe6c74c33c6e4ea7369a1842
Christian Krämer <christian.kraemer@tngtech.com>
2017-09-28 22:10:47 +0200

    dfplayer base version

--- dfplayer.lua
+++ dfplayer.lua
@@ -6,46 +6,59 @@ local VERSION = 0xFF
 local LENGTH = 0x06
 local FEEDBACK = 0x01
 
-function init_defaults()
-    init(5, 4, 16)
-end
 
-function init(tx_pin, rx_pin, busy_pin)
+function dfplayer.init(tx_pin, rx_pin, busy_pin)
     softuart.setup(9600, tx_pin, tx_pin);
     softuart.on("data", 1, function(data) print("lua handler called"); print(data) end);
 end
 
-function print_table(table)
+function dfplayer.init_default()
+    dfplayer.init(5, 4, 16)
+end
+
+function dfplayer.print_table(table)
     local output = "{"
     for k,v in pairs(table) do
-        output = output .. " " .. k .. "= " .. v .. ", "
+        output = output .. " " .. k .. " = " .. v .. ", "
     end
     output = output .. "}"
     print(output)
 end
 
+function dfplayer.split_number(number)
+    return bit.band(bit.rshift(number, 8), 0xFF), bit.band(number, 0xFF)
+end
+
 function dfplayer.calc_checksum(cmd, param1, param2)
     checksum = -(VERSION + LENGTH + cmd + FEEDBACK + param1 + param2)
-    return bit.rshift(checksum, 8), bit.band(checksum, 0xFF)
+    return dfplayer.split_number(checksum)
 end
 
 function dfplayer.send_command(cmd, param1, param2)
-    sum1, sum2 = calc_checksum(cmd, param1, param2)
-    message={0x7E, VERSION, LENGTH, cmd, FEEDBACK, param1, param2, sum1, sum2, 0xEF}
-    send_seq(message)
+    sum1, sum2 = dfplayer.calc_checksum(cmd, param1, param2)
+    message = {0x7E, VERSION, LENGTH, cmd, FEEDBACK, param1, param2, sum1, sum2, 0xEF}
+    dfplayer.send_seq(message)
 end
 
 function dfplayer.send_seq(seq)
     for _, s in pairs(seq) do
+        print(string.format("%x", s))
         softuart.write(s);
     end
 end
 
-function dfplayer.play()
-    send_command(0x03, 00, 00)
+function dfplayer.next()
+    dfplayer.send_command(0x01, 00, 00)
 end
 
+function dfplayer.prev()
+    dfplayer.send_command(0x02, 00, 00)
+end
 
+function dfplayer.play(track)
+    low, high = dfplayer.split_number(track)
+    dfplayer.send_command(0x03, low, high)
+end
 
 return dfplayer
 
